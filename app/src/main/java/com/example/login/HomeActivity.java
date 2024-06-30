4dd97940f352ca3a235f278944007b0ee9f322c3
xoyulkim <pejkjh02@naver.com>
2024-07-01 00:22:54 +0900

    홈 화면 수정

--- app/src/main/java/com/example/login/HomeActivity.java
+++ app/src/main/java/com/example/login/HomeActivity.java
@@ -1,4 +1,25 @@
+// HomeActivity.java
 package com.example.login;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ImageView searchIcon = findViewById(R.id.search_icon);
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, com.example.login.SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
