47837fc9103efd42d33ded7a25f39e5156c512d0
xoyulkim <pejkjh02@naver.com>
2024-07-07 17:30:27 +0900

    branch 바꾸기 전

--- app/src/main/java/com/example/login/HomeActivity.java
+++ app/src/main/java/com/example/login/HomeActivity.java
@@ -1,25 +1,32 @@
-// HomeActivity.java
 package com.example.login;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private EditText editTextPhoneNumber;
+    private ImageView search_icon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ImageView searchIcon = findViewById(R.id.search_icon);
-        searchIcon.setOnClickListener(new View.OnClickListener() {
+        search_icon = findViewById(R.id.search_icon);
+        search_icon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, com.example.login.SearchActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/com/example/login/SearchActivity.java
+++ app/src/main/java/com/example/login/SearchActivity.java
@@ -1,30 +1,17 @@
-// SearchActivity.java
 package com.example.login;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-
-        ImageView backButton = findViewById(R.id.back_button);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 홈 액티비티로 돌아가는 인텐트 설정
-                Intent intent = new Intent(SearchActivity.this, HomeActivity.class);
-                // 이전 액티비티들을 모두 종료하고 새로운 홈 액티비티를 시작
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                finish(); // 현재 액티비티를 종료
-            }
-        });
+        setContentView(R.layout.activity_home);
     }
 }
-
