1fd0c0f7e4a4d608f274f8edf629dc4a2c339147
skaguswjd0212 <skaguswjd0212@naver.com>
2024-11-03 10:40:03 +0900

    fix: add productId, sellerId

--- app/src/main/java/com/example/login/model/ProductDetailsResponse.kt
+++ app/src/main/java/com/example/login/model/ProductDetailsResponse.kt
@@ -3,6 +3,8 @@ package com.example.login.model
 import java.time.LocalDateTime
 
 data class ProductDetailsResponse(
+    val productId: Long,
+    val sellerId: Long,
     val userName: String,
     val imageUrl: String,
     val title: String,
